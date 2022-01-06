49d2f73699d0fa9d0d96507f3933b4278650267a
Somdada <somdada2025@gmail.com>
2022-01-06 15:00:06 +0900

    220106

--- asset/js/script.js
+++ asset/js/script.js
@@ -41,14 +41,69 @@ $(function(){
   }
 
     // 상품 탭
+    // 메인 상품 메뉴를 클릭하면, 메뉴의 데이터탭 값을 가져와서 tab_id에 저장
   $('.main-product ul li').click(function(){
     var tab_id = $(this).attr('data-tab');
-
+    //active가 들어가있는 메뉴,콘텐츠 영역에 active 지워주기 
     $('.main-product ul li').removeClass('active');
     $('.main-product .con-area').removeClass('active');
+    // 클릭된 메뉴에 active 클래스 넣어주고
+    // 받아온 (#+데이터탭)아이디 값에 active 넣어주기 
+    $(this).addClass('active');
+    $("#"+tab_id).addClass('active');
+  });
+
+    // 라이프 탭
+  $('.main-life ul li').click(function(){
+    var tab_id = $(this).attr('data-tab');
+
+    $('.main-life ul li').removeClass('active');
+    $('.main-life .con-area').removeClass('active');
 
     $(this).addClass('active');
     $("#"+tab_id).addClass('active');
+  });
+
+  // 공지사항 롤링
+  jQuery(function($)
+  {
+      var ticker = function()
+      {
+          timer = setTimeout(function(){
+              $('.notice-area .rolling li:first').animate( {marginTop: '-30px'}, 600, function() {
+                  $(this).detach().appendTo('.rolling').removeAttr('style');
+              });
+              ticker();
+          }, 3000);
+      };
+      // 이전
+      $(document).on('click','.notice-btn button.up',function() {
+          $('.notice-area .rolling li:last').hide().prependTo($('.rolling')).slideDown();
+          clearTimeout(timer);
+          ticker();
+      });
+
+      // 다음
+      $(document).on('click','.notice-btn button.down',function(){
+          $('.notice-area .rolling li:first').animate( {marginTop: '-30px'}, 400, function() {
+              $(this).detach().appendTo('.rolling').removeAttr('style');
+          });
+          clearTimeout(timer);
+          ticker();
+      });
+
+      // 마우스를 올렸을 때 기능 정지
+      var tickerover = function(event)
+      {
+          $('.rolling').mouseover(function(){
+              clearTimeout(timer);
+          });
+          $('.rolling').mouseout(function(){
+              ticker();
+          });
+      };
+      tickerover();
+      ticker();
   });
 
 
